f110b79e72acd3b9915bbad4ea2a595bca1f9857
CarlosPenaforte <dannhl08@gmail.com>
2022-06-21 14:17:00 -0300

    Back to old website

--- src/components/Footer/FooterButton/FooterButton.js
+++
@@ -1,9 +0,0 @@
-const FooterButton = (props) => {
-  const { rel, href, target, name, className } = props;
-
-  return(
-    <a className={className} rel={rel} href={href} target={target}>{name}</a>
-  )
-}
-
-export default FooterButton
